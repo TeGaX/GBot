ef0f28c1ca91c5d710a1f6606e0d0204ab7366f0
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-03-31 13:10:00 +0400

    userbot: modules: kang, time: fixups
    * Fixed kang pack numbers and sticker pack creation
    * Fixed time command not working for some countries, also added .title() to all commands.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- userbot/modules/time.py
+++ userbot/modules/time.py
@@ -15,6 +15,20 @@ COUNTRY = ''
 
 """ returns the timezone for a given country """
 async def get_tz(con):
+    """ Get time zone of the given country. """
+    if "(Uk)" in con:
+        con = con.replace("Uk", "UK")
+    if "(Us)" in con:
+        con = con.replace("Us", "US")
+    if " Of " in con:
+        con = con.replace(" Of ", " of ")
+    if "(Western)" in con:
+        con = con.replace("(Western)", "(western)")
+    if "Minor Outlying Islands" in con:
+        con = con.replace("Minor Outlying Islands","minor outlying islands")
+    if "Nl" in con:
+        con  = con.replace("Nl", "NL")
+
     for c_code in c_n:
         if con == c_n[c_code]:
             return tz(c_tz[c_code][0])
@@ -29,7 +43,7 @@ async def get_tz(con):
 async def time_func(tdata):
     if not tdata.text[0].isalpha() and tdata.text[0] not in ("/", "#", "@", "!"):
         con = tdata.pattern_match.group(1).title()
-        t_form = "%l:%M %p"
+        t_form = "%I:%M %p"
 
         if not con:
             if not COUNTRY:
@@ -51,7 +65,29 @@ async def time_func(tdata):
 @register(outgoing=True, pattern="^.date$")
 async def date_func(dat):
     if not dat.text[0].isalpha() and dat.text[0] not in ("/", "#", "@", "!"):
-        d_form="%d/%m/%y - %A"
+        d_form = "%d/%m/%y - %A"
+        con = dat.pattern_match.group(1).title()
+
+        if not con:
+            if not COUNTRY:
+                await dat.edit(f"`It's`  **{dt.now().strftime(d_form)}**  `here.`")
+                return
+
+            time_zone = await get_tz(COUNTRY)
+            await dat.edit(
+                f"`It's`  **{dt.now(time_zone).strftime(d_form)}**  `here, in {COUNTRY}.`"
+            )
+            return
+
+        time_zone = await get_tz(con)
+        if not time_zone:
+            await dat.edit("``` Wrong country given! Try again! ```")
+            return
+
+        try:
+            c_name = c_n[con]
+        except KeyError:
+            c_name = con
 
         await dat.edit("`it's`  **"+dt.now().strftime(d_form)+"**")
 
@@ -60,7 +96,7 @@ async def set_time_country(loc):
     """ For .ctime command, change the default userbot country for date and time commands. """
     if not loc.text[0].isalpha() and loc.text[0] not in ("/", "#", "@", "!"):
         global COUNTRY
-        temp_country = loc.pattern_match.group(1)
+        temp_country = loc.pattern_match.group(1).title()
 
         time_zone = await get_tz(temp_country.title())
         if not time_zone:
